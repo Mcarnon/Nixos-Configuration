b23d03b5acb4b83c9e5fb3f68dc4355693138c6a
Mcarnon <3273556124@qq.com>
2026-08-25 01:45:46 +0800

    Fix audio driver

--- modules/audio.nix
+++ modules/audio.nix
@@ -7,18 +7,12 @@ let
       mkdir -p $out/lib/firmware/intel/sof
       mkdir -p $out/lib/firmware/intel/sof-tplg
 
-      # 复制固件（实际文件在 intel-signed 子目录）
-      cp -L ${pkgs.sof-firmware}/lib/firmware/intel/sof/intel-signed/sof-tgl.ri.zst $out/lib/firmware/intel/sof/
-      # 复制拓扑（在 sof-tplg 目录下）
+      cp -L ${pkgs.sof-firmware}/lib/firmware/intel/sof/sof-tgl.ri.zst $out/lib/firmware/intel/sof/
       cp -L ${pkgs.sof-firmware}/lib/firmware/intel/sof-tplg/sof-tgl-es8336.tplg.zst $out/lib/firmware/intel/sof-tplg/
 
-      # 解压固件
       zstd -d $out/lib/firmware/intel/sof/sof-tgl.ri.zst -o $out/lib/firmware/intel/sof/sof-tgl.ri
-
-      # 解压拓扑并重命名为驱动期望的名称
       zstd -d $out/lib/firmware/intel/sof-tplg/sof-tgl-es8336.tplg.zst -o $out/lib/firmware/intel/sof-tplg/sof-tgl-es8336-dmic2ch.tplg
 
-      # 删除压缩文件，避免干扰
       rm $out/lib/firmware/intel/sof/sof-tgl.ri.zst
       rm $out/lib/firmware/intel/sof-tplg/sof-tgl-es8336.tplg.zst
     '';
