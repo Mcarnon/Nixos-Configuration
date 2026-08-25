0ad63279d9654a4f6aa90ea19b824bf911559a09
Mcarnon <3273556124@qq.com>
2026-08-25 17:58:10 +0800

    Bugfixed

--- checks/miyu.nix
+++ checks/miyu.nix
@@ -17,7 +17,8 @@ pkgs.testers.nixosTest {
   testScript = ''
     machine.wait_for_unit("multi-user.target")
     machine.succeed("test -x /run/current-system/sw/bin/miyu")
-    machine.succeed("miyu --help | grep -q \"shell-intercept\"")
+    # shell-intercept / shell-classify are hide=true in src/cli/args.rs:28, never in --help
+    machine.succeed("miyu --shell-classify --shell fish 'echo hi'")
     machine.succeed("test -f /etc/fish/conf.d/zz-miyu.fish")
     # Smoke: hook loads without error in fish
     machine.succeed("fish -c 'source /etc/fish/conf.d/zz-miyu.fish; functions -q __miyu_wrap_fish_prompt && echo ok' | grep -q ok")
