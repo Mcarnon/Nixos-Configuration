edd44065638fccf0a18548098694acb560825de6
Mcarnon <3273556124@qq.com>
2026-08-27 23:16:21 +0800

    Bug fixed

--- modules/home/apps/gui.nix
+++ modules/home/apps/gui.nix
@@ -11,6 +11,7 @@
     # -- Essentials --
     foot # Wayland terminal emulator
     nautilus # file manager (niri's portal file picker also depends on it)
+    gvfs # trash backend + remote/removable mounts support for nautilus
     inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default # browser
     xdg-utils # xdg-open & friends
 
